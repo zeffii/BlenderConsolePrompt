da2d8051783e844e709620b814c5db20e83ddeaa
zeffii <tetha.z@gmail.com>
2015-02-19 13:24:26 +0100

    graphs only returns the last graph..

--- addons_contrib/BCPrompt/bc_utils.py
+++ addons_contrib/BCPrompt/bc_utils.py
@@ -68,26 +68,57 @@ def get_sv_times():
 
     # at this point sverchok should be available.
     # github.com/nortikin/sverchok/issues/500#issuecomment-67337023
-    def write_time_graph_json(destination_path):
+    def write_time_graph_json(destination_path, dp2):
+
+        # trigger a final update.
+        bpy.ops.node.sverchok_update_all()
+
         m = sverchok.core.update_system.graphs
-        print(m)
+        mn = sverchok.core.update_system.graph_names
+        print('graph names:', mn)
 
-        atk = {}
-        for idx, event in enumerate(m[0]):
-            atk[idx] = event
+        ''' Original single shot file'''
+        if True:
 
-        tk = dict(items=atk)
-        tkjson = json.dumps(tk, sort_keys=True, indent=2)
-        print('tkjson: ', type(tkjson))
-        # import pprint
-        # pprint.pprint(tkjson)
-        with open(destination_path, 'w') as time_graph:
+            atk = {}
+            for idx, event in enumerate(m[0]):
+                atk[idx] = event
 
-            # this augments the first line of the json with a var
-            # transporming it into a valid .js file which can be
-            # called from the inlet.js
-            final_write = "var jsonObject = " + tkjson
-            time_graph.writelines(final_write)
+            tk = dict(items=atk)
+            tkjson = json.dumps(tk, sort_keys=True, indent=2)
+
+            # import pprint
+            # pprint.pprint(tkjson)
+            with open(destination_path, 'w') as time_graph:
+
+                # this augments the first line of the json with a var
+                # transporming it into a valid .js file which can be
+                # called from the inlet.js
+                final_write = "var jsonObject = " + tkjson
+                time_graph.writelines(final_write)
+
+        ''' Augmented full node tree and subtree json'''
+
+        if True:
+            full_atk = {}
+            print('number of subgraphs:', len(m))
+            for index, (graph, graph_name) in enumerate(zip(m, mn)):
+                print('-----', index, graph, graph_name, '<<<<')
+
+                atk = {idx: event for idx, event in enumerate(graph)}
+                gtk = dict(items=atk, name=graph_name)
+                full_atk[index] = gtk
+
+            print(full_atk)
+
+            tkjson_full = json.dumps(full_atk, sort_keys=True, indent=2)
+            with open(dp2, 'w') as time_graph:
+
+                # this augments the first line of the json with a var
+                # transporming it into a valid .js file which can be
+                # called from the inlet.js
+                final_write = "var jsonObject = " + tkjson_full
+                time_graph.writelines(final_write)
 
     for g in ng:
         if g.bl_idname == 'SverchCustomTreeType':
@@ -96,7 +127,8 @@ def get_sv_times():
     if ng:
         _root = os.path.dirname(__file__)
         fp = os.path.join(_root, 'tmp', 'sverchok_times.json')
-        write_time_graph_json(fp)
+        fp_full = os.path.join(_root, 'tmp', 'sverchok_times_full.json')
+        write_time_graph_json(fp, fp_full)
         bcp_webbrowser('index.html')
     else:
         print(ng, 'node_groups not found..')
