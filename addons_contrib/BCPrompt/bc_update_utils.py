8d722a0856325fb7d668707701ff40d96c7e2532
zeffii <tetha.z@gmail.com>
2015-02-23 18:46:06 +0100

    this will show the filename

--- addons_contrib/BCPrompt/bc_update_utils.py
+++ addons_contrib/BCPrompt/bc_update_utils.py
@@ -6,24 +6,29 @@ from urllib.request import urlopen
 def peek_builder_org(search_target):
     base_url = 'https://builder.blender.org/download/'
     print('reading from', base_url)
-    # d = urlopen(base_url)
-    # d = d.read()
+    d = urlopen(base_url)
+    d = d.read().decode('utf8')
+    d = d.split('\n')
 
-    rpath = r'C:\Users\dealga\Desktop\panz.html'
-    with open(rpath) as f:
-        d = f.readlines()
-        hrefs = []
-        pattern = 'href=\"(.*)\"\>'
+    # #print(d)
 
-        for line in d:
-            if ('href=' in line):
-                if all([(target in line) for target in search_target]):
-                    full_line = line.strip()
-                    match = re.search(pattern, full_line)
-                    href_str = match.group(1)
-                    hrefs.append(href_str)
+    # #rpath = r'C:\Users\dealga\Desktop\panz.html'
+    # #with open(rpath) as f:
+    # #    d = f.readlines()
 
-        for ref in hrefs:
-            print(ref)
+    hrefs = []
+    pattern = 'href=\"(.*)\"\>'
 
-        return hrefs if hrefs else None
+    for line in d:
+        if ('href=' in line):
+            if all([(target in line) for target in search_target]):
+                full_line = line.strip()
+                match = re.search(pattern, full_line)
+                href_str = match.group(1)
+                hrefs.append(href_str)
+
+    return hrefs if hrefs else None
+
+# if True:
+#     f = peek_builder_org(['win32', '>ble'])
+#     print(f
