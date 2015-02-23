882db71a4556a6ccbb4834ec11e498cda836468e
zeffii <tetha.z@gmail.com>
2015-02-23 19:04:44 +0100

    elaborate design specs

--- addons_contrib/BCPrompt/bc_update_utils.py
+++ addons_contrib/BCPrompt/bc_update_utils.py
@@ -2,6 +2,37 @@
 import re
 from urllib.request import urlopen
 
+'''
+design considerations
+
+from past experience this should not totally flatten the existing install,
+rather it it should copy the content (minus folders..2.7x etc) into a
+new archive. So in the event of 'catastrophic' failure I can revert.
+
+Further caution taken with regards to any symlinked folders which may exist.
+For me inside addons and addons_contrib i have several such folders which
+link to local git repository of addons in development.
+
+I think the process should be
+- locate download zip (After narrow down)
+- start external python thread
+- tell it where the current .exe is
+- close blender
+- zip all files in the executable folder
+- read whitelist of addons to remove from the download zip before copy
+in my case
+    - addons/sverchok
+    - addons_contrib/BioBlender
+    - addons_contrib/tinyCAD
+    - addons_contrib/BCPrompt
+    - any more?
+- remove all whitelisted folders from the download.zip
+- unpack download.zip into correct place
+- start blender
+- end longrunning python thread
+
+'''
+
 
 def peek_builder_org(search_target):
     base_url = 'https://builder.blender.org/download/'
