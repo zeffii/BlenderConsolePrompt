33a3706f3cafc03c8e0fe13518b6199a1d3f867b
zeffii <tetha.z@gmail.com>
2015-03-09 09:37:04 +0100

    now does faux intersection, I find this useful already

--- addons_contrib/BCPrompt/bc_CAD_utils.py
+++ addons_contrib/BCPrompt/bc_CAD_utils.py
@@ -1,20 +1,21 @@
 import bpy
 import bmesh
 from mathutils.geometry import intersect_ray_tri
+from mathutils.geometry import intersect_point_line
 from mathutils.geometry import tessellate_polygon as tessellate
 
 
 def perform_face_intersection():
 
-    def rays_from_active_face(face):
+    def rays_from_face(face):
         '''
-        for every edge returns bidirectional (v1,v2), (v2,v1)
-        - in case the face intersection happens on the origin of the ray
+        per edge (v1, v2) this returns the reverse edge too (v2, v1)
+        in case the face intersection happens on the origin of the ray
         '''
         indices = [v.index for v in face.verts]
         indices.append(indices[0])  # makes cyclic
-        edges_f = [[indices[i], indices[i+1]] for i in range(len(indices)-1)]  # forward
-        edges_b = [[indices[i+1], indices[i]] for i in range(len(indices)-1)]  # backward
+        edges_f = [(indices[i], indices[i+1]) for i in range(len(indices)-1)]  # forward
+        edges_b = [(indices[i+1], indices[i]) for i in range(len(indices)-1)]  # backward
         return edges_f + edges_b
 
     def triangulated(face):
@@ -31,30 +32,50 @@ def perform_face_intersection():
     bm.verts.ensure_lookup_table()  # get 2.73+
     bm_verts = bm.verts
     bm_faces = bm.faces
+
+    # Prime Face
     active = bm_faces.active
 
-    test_rays = rays_from_active_face(active)
-    faces_to_intersect = get_selected_minus_active(bm_faces, active.index)
+    # triangulate face to intersect
+    tris_to_intersect = triangulated(active)  # list of collections of 3 coordinates
+    av = active.verts
+    tris = [[av[idx1].co, av[idx2].co, av[idx3].co] for idx1, idx2, idx3 in tris_to_intersect]
+
+    # this will hold the set of edges used to raycast, using set will avoid many duplicates of touching
+    # faces that share edges.
+    test_rays = set()
+
+    # check intersection with bidirectional of all edges of all faces that are selected but not active
+
+    # Non Prime Faces
+    faces_to_intersect_with = get_selected_minus_active(bm_faces, active.index)
+    for face in faces_to_intersect_with:
+        rays = rays_from_face(face)
+        for ray in rays:
+            test_rays.add(ray)
 
     vert_set = set()
-    ct = 0
-    for f in faces_to_intersect:
-        print('intersect test')
-        for tri in triangulated(f):
-            print(ct, tri)
-            ct += 1
-            idx1, idx2, idx3 = tri
-            v1, v2, v3 = f.verts[idx1].co, f.verts[idx2].co, f.verts[idx3].co
-            for ray_idx, orig_idx in test_rays:
-                orig = bm_verts[orig_idx].co
-                ray = (bm_verts[ray_idx].co - orig).normalized()
-                res = intersect_ray_tri(v1, v2, v3, ray, orig)
-                if res:
-                    print('res:', res)
-                    vert_set.add(res[:])
-        print('----')
 
-    print('found {0} unique verts'.format(len(vert_set)))
+    for v1, v2, v3 in tris:
+
+        for ray_idx, orig_idx in test_rays:
+            orig = bm_verts[orig_idx].co
+            ray_original = bm_verts[ray_idx].co
+            ray = (bm_verts[ray_idx].co - orig).normalized()
+            pt = intersect_ray_tri(v1, v2, v3, ray, orig)
+            if pt:
+                # print('res:', pt)
+
+                # filter new verts, they must lie on the edges of the
+                # if res lies on line described by (origin, ray_original) then add.
+                itx_res = intersect_point_line(pt, ray_original, orig)
+                if itx_res:
+                    # print(itx_res)
+                    v, dist = itx_res
+                    if (0.0 < dist < 1.0):
+                        vert_set.add(pt[:])
+
+    print('found {0} unique new verts'.format(len(vert_set)))
 
     for v in vert_set:
         bm.verts.new(v)
