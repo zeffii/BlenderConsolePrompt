ac0401d57b1af1ecd8e9eb0a5a42a3ed2beb02be
zeffii <tetha.z@gmail.com>
2019-09-03 19:30:23 +0200

    rename _PT_

--- addons_contrib/BCPrompt/bc_panels.py
+++ addons_contrib/BCPrompt/bc_panels.py
@@ -1,9 +1,9 @@
 import bpy
 
 
-class TextSyncPanel(bpy.types.Panel):
+class Text_PT_SyncPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
-    bl_idname = "TextSyncPanel"
+    bl_idname = "Text_PT_SyncPanel"
     bl_label = "text sync checker"
     bl_space_type = "TEXT_EDITOR"
     bl_region_type = "UI"
@@ -23,5 +23,5 @@ class TextSyncPanel(bpy.types.Panel):
         if context.edit_text.is_modified:
             row.operator("text.text_upsync")
 
-classes = [TextSyncPanel]
+classes = [Text_PT_SyncPanel]
 register, unregister = bpy.utils.register_classes_factory(classes)
